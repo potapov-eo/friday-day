cdcc6b084f1e1fba3fe540fe4b6fe8cd24dad08d
potapov-eo <potapov.eo@yandex.ru>
2021-02-08 12:17:56 +0300

    Preloader

--- src/n1-main/m1-ui/common/Preloder/Preloader.tsx
+++ src/n1-main/m1-ui/common/Preloder/Preloader.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import style from './Preloader.module.css'
 
 
 let Preloader = () => {
-    return <div  >
+    return <div className={style.pre} >
         <img src="https://media.giphy.com/media/wnYB3vx9t6PXiq1ubB/giphy.gif"  style={ { height: 200 } }/>
     </div>
 }
